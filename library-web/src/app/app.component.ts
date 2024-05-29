a72eda22a2c03437fa204ce652da10b004b37eb5
Cyh <2333973@cognizant.com>
2024-05-29 16:47:04 +0800

    Added add account and login button to login page

--- library-web/src/app/app.component.ts
+++ library-web/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatFormFieldModule, MatInputModule],
+  imports: [RouterOutlet, MatToolbarModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
