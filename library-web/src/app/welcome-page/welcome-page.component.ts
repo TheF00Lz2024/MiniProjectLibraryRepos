d9e0a7b52c59d3a1a95ef1c5d1d521068fb445f5
Cyh <2333973@cognizant.com>
2024-07-16 14:29:20 +0800

    Fixed the issue with the side nav bar regarding the spaces to % for the height

--- library-web/src/app/welcome-page/welcome-page.component.ts
+++ library-web/src/app/welcome-page/welcome-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-welcome-page',
   standalone: true,
-  imports: [MatSidenavModule],
+  imports: [
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.css'
 })
