908dd139d8da1fff0557adb0ebeaa9e4841744ef
Cyh <2333973@cognizant.com>
2024-07-17 18:12:37 +0800

    no changes

--- library-web/src/app/view-all-user/view-all-user.component.ts
+++ library-web/src/app/view-all-user/view-all-user.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-view-all-user',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './view-all-user.component.html',
   styleUrl: './view-all-user.component.css'
 })
 export class ViewAllUserComponent {
-
 }
