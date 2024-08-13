443d452fd603e6c4fb7c4b55db0093cfc47130ae
Cyh <2333973@cognizant.com>
2024-08-13 12:19:18 +0800

    Added 3 new filter such as filter by isbn, title and author. Tested the filter working and also added custom message for it

--- library-web/src/app/view-all-book/view-all-book.component.ts
+++ library-web/src/app/view-all-book/view-all-book.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,8 +9,9 @@ import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSortModule, MatSort } from '@angular/material/sort';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
-import { bookData, formattedUser } from '../model/apiResponse';
+import { bookData } from '../model/apiResponse';
 import { LibraryApiService } from '../service/library-api.service';
+import {MatTabChangeEvent, MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-view-all-book',
@@ -25,23 +26,33 @@ import { LibraryApiService } from '../service/library-api.service';
     CommonModule,
     MatProgressSpinnerModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTabsModule
   ],
   templateUrl: './view-all-book.component.html',
   styleUrl: './view-all-book.component.css'
 })
 export class ViewAllBookComponent {
 
-  //show loading effect
+  // show loading effect
   showLoading: boolean = false;
 
   // set up the column name for the table
   displayedColumns: string[] = ['isbn', 'title', 'authorName'];
 
+  // store placeholder value for filter
+  plcaeHolder: string = '978-3-16-148410-0';
+
+  // store filter selection
+  filterSelection: string = 'isbn';
+
   dataSource!: MatTableDataSource<bookData>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+
+  @ViewChild('input')
+  filterValue!: ElementRef;
 
   //bind the table render to ng OnInit event
   ngOnInit(): void {
@@ -52,13 +63,42 @@ export class ViewAllBookComponent {
   // filter event to filter by username
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filterPredicate = function (data, filter: string): boolean {
-      return data.title.includes(filter);
-    };
+    if(this.filterSelection == 'isbn'){
+      this.dataSource.filterPredicate = function (data, filter: string): boolean {
+        return data.isbn.includes(filter);
+      };
+    }else if(this.filterSelection == 'title'){
+      this.dataSource.filterPredicate = function (data, filter: string): boolean {
+        return data.title.includes(filter);
+      };
+    }else{
+      this.dataSource.filterPredicate = function (data, filter: string): boolean {
+        return data.authorName.includes(filter);
+      };
+    }
     this.dataSource.filter = filterValue.trim();
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
+  }
+
+  // listen to filter tab event
+  tabEvent(event: MatTabChangeEvent){
+    if(event.index == 0){
+      this.filterSelection = 'isbn';
+      this.plcaeHolder = '978-3-16-148410-0';
+    } else if(event.index == 1){
+      this.filterSelection = 'title';
+      this.plcaeHolder = 'Little Red Riding Hood';
+    } else{
+      this.filterSelection = 'author name';
+      this.plcaeHolder = 'John Doe';
+    }
+    // reset filter value
+    this.filterValue.nativeElement.value = '';
+    // reset filter
+    this.dataSource.filter = "";
+
   }
 
   //create constructor to build form, use custom services
